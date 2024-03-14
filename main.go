5df363b0cc4680c362787bf187c0720246664e96
Raymond Perminov <raymondproduction@ukr.net>
2024-03-15 01:19:28 +0200

    Open other db by dialog

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"github.com/gotk3/gotk3/gtk"
 )
 
+var currentDatabasePath = "./songs.db"
+
 func main() {
 
 	gtk.Init(nil)
@@ -66,10 +68,21 @@ func initGTKWindow() *gtk.Window {
 		}
 		defer dialog.Destroy()
 
+		filter, err := gtk.FileFilterNew()
+		if err != nil {
+			log.Fatal("Failed to create filter:", err)
+		}
+		filter.AddPattern("*.db") // Filter for database files
+		dialog.AddFilter(filter)
+
 		response := dialog.Run()
 		if response == gtk.RESPONSE_ACCEPT {
-			filename := dialog.GetFilename()
-			log.Println("Selected file:", filename)
+			currentDatabasePath := dialog.GetFilename()
+			getComboboxById("select-song-1", builder).RemoveAll()
+			getComboboxById("select-song-2", builder).RemoveAll()
+			getComboboxById("select-song-3", builder).RemoveAll()
+			populateSongsAsync(builder)
+			log.Println("Selected file:", currentDatabasePath)
 		}
 	})
 
@@ -116,7 +129,7 @@ func loadSongsFromDatabase() ([]string, error) {
 	// 	statement.Exec("Song 4", "Classical", "2023-03-04", "http://example.com/song4")
 	// 	statement.Exec("Song 5", "Electronic", "2023-03-05", "http://example.com/song5")
 
-	database, err := sql.Open("sqlite", "./songs.db")
+	database, err := sql.Open("sqlite", currentDatabasePath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open database: %v", err)
 	}
@@ -157,23 +170,33 @@ func populateSongsAsync(builder *gtk.Builder) {
 
 		// Safe update of the GUI in the main thread
 		glib.IdleAdd(func() bool {
-			populateComboBox(builder, songs)
+			populateComboBox("select-song-1", builder, songs)
+			populateComboBox("select-song-2", builder, songs)
+			populateComboBox("select-song-3", builder, songs)
 			return false // We return false so that the function is executed only once
 		})
 	}()
 }
 
-func populateComboBox(builder *gtk.Builder, songs []string) {
-	obj, err := builder.GetObject("select-song-1")
-	if err != nil {
-		log.Fatal("Could not find 'select-song-1'", err)
-	}
-	sel, ok := obj.(*gtk.ComboBoxText)
-	if !ok {
-		log.Fatal("Failed to get 'select-song-1' as ComboBoxText")
-	}
+func populateComboBox(widgetID string, builder *gtk.Builder, songs []string) {
+
+	comboBoxText := getComboboxById(widgetID, builder)
 
 	for _, name := range songs {
-		sel.AppendText(name)
+		comboBoxText.AppendText(name)
 	}
+}
+
+func getComboboxById(widgetID string, builder *gtk.Builder) *gtk.ComboBoxText {
+	obj, err := builder.GetObject(widgetID)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Could not find '%s'", widgetID), err)
+	}
+	comboBoxText, ok := obj.(*gtk.ComboBoxText)
+
+	if !ok {
+		log.Fatal(fmt.Sprintf("Failed to get '%s' as ComboBoxText", widgetID))
+	}
+
+	return comboBoxText
 }
